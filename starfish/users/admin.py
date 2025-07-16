66797519e8a9311724399af9181d94d5fddd12c2
Jeff Fein-Worton <jeff@fein-worton.com>
2025-07-15 22:59:59 -0700

    added ChapterRoleInline to UserAdmin

--- starfish/users/admin.py
+++ starfish/users/admin.py
@@ -1,9 +1,10 @@
+from chapters.models import ChapterRole
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 from simple_history.admin import SimpleHistoryAdmin
-from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, TabularInline
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 
 from .models import User
@@ -14,6 +15,22 @@ admin.site.unregister(Group)
 @admin.register(Group)
 class GroupAdmin(BaseGroupAdmin, ModelAdmin):
     compressed_fields = True
+
+
+class ChapterRoleInline(TabularInline):
+    model = ChapterRole
+    fk_name = 'user'
+    fields = ['chapter', 'role', 'added_by_user']
+    readonly_fields = fields
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 @admin.register(User)
@@ -49,3 +66,4 @@ class UserAdmin(BaseUserAdmin, SimpleHistoryAdmin, ModelAdmin):
     search_fields = ('username', 'email', 'first_name', 'last_name')
     ordering = ('username',)
     compressed_fields = True
+    inlines = [ChapterRoleInline]
