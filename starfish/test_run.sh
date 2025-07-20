dda71556bad8912836e2e3aa9a1bb17e9493b607
lux-u-tenebris <lux.u.tenebris@gmail.com>
2025-07-20 15:37:46 -0500

    add checks for scripts

--- starfish/test_run.sh
+++ starfish/test_run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Be sure you ran dev_setup.sh first." >&2
+if [ ! -e ".env" ]; then
+  echo "Run install.sh and configure .env file first." >&2
+  exit 1
+fi
 
 source ./.env
+
 python manage.py test
